63ecb990cda8b288d43d845a7e83cd2a7ca2fd2f
alex.query <alex.query@gmail.com>
2015-02-23 22:15:10 +0000

    tests

--- src/br/net/underdesk/main/view/main.js
+++ src/br/net/underdesk/main/view/main.js
@@ -1,3 +1,5 @@
 $(function(){   
-	_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/"]});
+	 var vls = [50,50,100,100]; 
+	 $("div.chartpie").peity("pie", { "width":100,"height":100,"fill": ["#ff9900", "#babaca","#487564","#0000cc"]}).text(vls.join(",")).change();
+	//_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/"]});
 });
