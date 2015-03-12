0a22425ee9d0125120332417093994ccd43d7cc9
alex.query <alex.query@gmail.com>
2015-03-12 22:04:49 +0000

    fixed view

--- src/br/net/underdesk/main/view/main.js
+++ src/br/net/underdesk/main/view/main.js
@@ -1,5 +1,3 @@
 $(function(){   
-	 var vls = [50,50,100,100]; 
-	 $("div.chartpie").peity("pie", { "width":100,"height":100,"fill": ["#ff9900", "#babaca","#487564","#0000cc"]}).text(vls.join(",")).change();
-	//_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/"]});
+	_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/"]});
 });
