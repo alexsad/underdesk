98baba1d5360cf3f40cb5c077b3d1eea2ac76d96
alex.query <alex.query@gmail.com>
2015-03-20 18:56:16 +0000

    fixed reload action

--- src/br/net/underdesk/main/view/main.js
+++ src/br/net/underdesk/main/view/main.js
@@ -1,5 +1,10 @@
 window.addEvent('domready',function(){ 
-	_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/assets/uml/"],"icon":"cd","title":"Arquivos"});
+	
+	
+	
+	
+	
+	//_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/assets/uml/"],"icon":"cd","title":"Arquivos"});
 
 
 	//_.loadModule({"mod":"Tabela","url":"js/br/net/underdesk/codigogerador/view/Tabela.js","act":"getTabelas","p":["/assets/uml/ata3_uml.json"],"icon":"indent-left","title":"*Geracao de Codigo"});
