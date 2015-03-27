6921b2e4f3b26ed2b3dd267eb7e0b37f12f78c58
alex.query <alex.query@gmail.com>
2015-03-27 21:33:39 +0000

    test with new array list class

--- src/br/net/underdesk/main/view/main.js
+++ src/br/net/underdesk/main/view/main.js
@@ -1,7 +1,7 @@
 window.addEvent('domready',function(){ 
 	
 	
-	
+
 	
 	
 	_.loadModule({"mod":"Arquivo","url":"js/br/net/underdesk/arquivo/view/Arquivo.js","act":"getByCaminho","p":["/assets/uml/"],"icon":"cd","title":"Arquivos"});
