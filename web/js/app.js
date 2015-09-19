2fe8103364e944565fac76cc175cd14852d56b5c
alexsad <alex.query@gmail.com>
2015-09-19 20:34:34 -0300

    testing configurations

--- web/js/app.js
+++ web/js/app.js
@@ -1,21 +1,24 @@
 requirejs.config({
-		baseUrl:'js/lib'
+		baseUrl:'js/br/net/underdesk/'
 		,urlArgs : "bust="+new Date().getTime()
 		,paths:{
 			br:'../br'
-			/*,util:'underas/util'
-			,core:'underas/core'
-			,container:'underas/container'
-			,controller:'underas/controller'
-			,net:'underas/net'*/
+				/*
+			,util:'util'
+			,core:'core'
+			,container:'../../../../../lib/container'
+			,controller:'controller'
+			,net:'net'
+			*/
 		}
 });
 
 //var _app = {"loaded":{}};
 
 $(function(){
+	/*
 	requirejs(
-		['container','br/underas/view/Arquivo']
+		['container','br/net/underdesk/arquivo/view/Arquivo']
 		,function(_container,_modtmp){
 			//console.log(m);
 			//var t = new sub.SubB(45);
@@ -28,4 +31,21 @@ $(function(){
 			mdw.append(teste); 
 		}
 	);	
+	*/
+	requirejs(
+			['arquivo/view/Arquivo']
+			,function(_modtmp){
+				//console.log(m);
+				//var t = new sub.SubB(45);
+				//t.doAnyThing("nova instancia!!!!");
+				//$("body").append("<div>!teste</div>");
+				var teste = new _modtmp.Arquivo();
+				/*
+				var mdw = new _container.ModView("cadastro de teste!!!");
+				mdw.setIcon("key");
+				mdw.show(true);
+				mdw.append(teste); 
+				*/
+			}
+		);	
 });
