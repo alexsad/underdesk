3d02b48f36566cb5a2396aaa9326ae50377f21b4
alexsad <alex.query@gmail.com>
2015-09-21 16:07:59 -0300

    add grunt works

--- src/br/net/underdesk/arquivo/view/Arquivo.ts
+++ src/br/net/underdesk/arquivo/view/Arquivo.ts
@@ -1,11 +1,14 @@
 import {ModWindow} from "../../../../../lib/container";
+import {ArrayList} from "../../../../../lib/util";
 import {Tabela} from "../../codigogerador/view/Tabela";
 export class Arquivo extends ModWindow{
     idArquivo:number;
     tb:Tabela;
+    tmpA:ArrayList;
     constructor(p_id:number){
-        super("janela teste","br.net.underdesk.arquivo.view.Arquivo");
+        super("janela teste3","br.net.underdesk.arquivo.view.Arquivo");
         this.idArquivo =   p_id;  
         this.tb = new Tabela();
+        this.tmpA = new ArrayList([]);
     }
 }
--- web/js/app.js
+++ web/js/app.js
@@ -1,22 +1,30 @@
 requirejs.config({
-		baseUrl:'js/br/net/underdesk/'
+		baseUrl:'js/lib'
 		,urlArgs : "bust="+new Date().getTime()
 		,paths:{
-			br:'../br'
-				/*
-			,util:'util'
-			,core:'core'
+			br:'../br'	
+			/*
+			,util:'../../../../../lib/util'
+			,core:'../../../../../lib/core'
 			,container:'../../../../../lib/container'
-			,controller:'controller'
-			,net:'net'
-			*/
+			,controller:'../../../../../lib/controller'
+			,net:'../../../../../lib/net'
+					
+			,'../../../../../lib/util':'util'
+			,'../../../../../lib/core':'core'
+			,'../../../../../lib/container':'container'
+			,'../../../../../lib/controller':'controller'
+			,'../../../../../lib/net':'net'
+			*/	
+				
+			
 		}
 });
 
 //var _app = {"loaded":{}};
 
 $(function(){
-	/*
+	
 	requirejs(
 		['container','br/net/underdesk/arquivo/view/Arquivo']
 		,function(_container,_modtmp){
@@ -25,27 +33,29 @@ $(function(){
 			//t.doAnyThing("nova instancia!!!!");
 			//$("body").append("<div>!teste</div>");
 			var teste = new _modtmp.Arquivo();
-			var mdw = new _container.ModView("cadastro de teste!!!");
+			var mdw = new _container.ModView("teste com amd");
 			mdw.setIcon("key");
 			mdw.show(true);
 			mdw.append(teste); 
 		}
 	);	
-	*/
+	
+	/*
 	requirejs(
-			['arquivo/view/Arquivo']
+			['br/net/underdesk/arquivo/view/Arquivo']
 			,function(_modtmp){
 				//console.log(m);
 				//var t = new sub.SubB(45);
 				//t.doAnyThing("nova instancia!!!!");
 				//$("body").append("<div>!teste</div>");
 				var teste = new _modtmp.Arquivo();
-				/*
-				var mdw = new _container.ModView("cadastro de teste!!!");
-				mdw.setIcon("key");
-				mdw.show(true);
-				mdw.append(teste); 
-				*/
+				
+				//var mdw = new _container.ModView("cadastro de teste!!!");
+				//mdw.setIcon("key");
+				//mdw.show(true);
+				//mdw.append(teste); 
+				
 			}
 		);	
+	*/
 });
